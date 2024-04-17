b9fd46d02f26ca3e7b7779abec77a6d65b0b3953
queenly9 <queenly9@naver.com>
2024-04-17 17:34:22 +0900

    Header 컴포넌트 작성

---
+++ src/component/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <>
+            <div
+                style={{ color: "white", backgroundColor: "lightblue", height: 70 }}>
+                Illusion
+            </div>
+        </>
+    );
+}
+
+export default Header;
