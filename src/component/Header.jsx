be318f78a00e80ba1294349c6637f092a95c307c
queenly9 <queenly9@naver.com>
2024-04-20 02:21:28 +0900

    헤더 수정 및 라우팅 작업

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import {useNavigate} from "react-router-dom";
 
 function Header() {
+    const navigate = useNavigate();
+
+    const goMainPage = () => {
+        navigate("/")
+    }
+    const goPost = () => {
+        navigate("/posts")
+    }
+    const goReview = () => {
+        navigate("/reviews")
+    }
+    const goTeam = () => {
+        navigate("/teams")
+    }
+    const goLogin = () => {
+        navigate("/login")
+    }
+
     return (
         <>
             <div
-                style={{ color: "white", backgroundColor: "lightblue", height: 70 }}>
-                Illusion
+                style={{ color: "white", backgroundColor: "lightblue", height: 70, display: "flex" }}>
+                <div onClick={goMainPage} style={{ width: 150, height: 70, display: "flex", alignItems: "center", justifyContent: "center", fontSize: 30, paddingRight: 400 }}>Illusion</div>
+                <div onClick={goPost} style={{ width: 100, height: 70, display: "flex", alignItems: "center", justifyContent: "center", fontSize: 18 }}>게시판</div>
+                <div onClick={goReview} style={{ width: 100, height: 70, display: "flex", alignItems: "center", justifyContent: "center", fontSize: 18 }}>리뷰</div>
+                <div onClick={goTeam} style={{ width: 100, height: 70, display: "flex", alignItems: "center", justifyContent: "center", fontSize: 18 }}>팀모집</div>
+                <div onClick={goLogin} style={{ width: 200, height: 70, display: "flex", alignItems: "center", justifyContent: "center", fontSize: 18, paddingLeft: 450, color: "grey" }}>로그인/회원가입</div>
             </div>
         </>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import MainPage from "./page/MainPage.jsx";
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import NotFound404 from "./page/error/NotFound404.jsx";
+import Posts from "./page/Posts.jsx";
+import Reviews from "./page/Reviews.jsx";
+import Teams from "./page/Teams.jsx";
+import Login from "./page/Login.jsx";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <NotFound404/>,
+        children: [
+            {path : "/", element: <MainPage/> }
+        ]
+    },
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <NotFound404/>,
+        children: [
+            {path : "/posts", element: <Posts/> }
+        ]
+    },
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <NotFound404/>,
+        children: [
+            {path : "/reviews", element: <Reviews/> }
+        ]
+    },
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <NotFound404/>,
+        children: [
+            {path : "/teams", element: <Teams/> }
+        ]
+    },
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <NotFound404/>,
+        children: [
+            {path : "/login", element: <Login/> }
+        ]
+    },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
